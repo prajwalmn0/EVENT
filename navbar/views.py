3953d4c27d4aa418d7575f08b0d525ae2d679aa2
unknown <prajwalmn3000@gmail.com>
2022-09-25 21:51:48 +0530

    'done'

---
+++ navbar/views.py
@@ -0,0 +1,7 @@
+from http.client import HTTPResponse
+from re import X
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
